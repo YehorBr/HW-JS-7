4a05fcee0a77be33c4b8fc60023d862efe73b466
Yehor <yehor12344321@gmail.com>
2024-03-05 17:19:38 +0200

    зберігає зміни

---
+++ index.js
@@ -0,0 +1,85 @@
+// const select = 'Сік';
+
+// switch (select) {
+//     case 'Кава':
+//         alert('Ви обрали каву.')
+//         break;
+    
+//     case 'Чай':
+//         alert('Ви обрали чай.')
+//         break;
+    
+//     case 'Сік':
+//         alert('Ви обрали сік.')
+//         break;    
+
+//     default:
+//         break;
+// }
+
+// const day = 'Неділя'
+
+// switch (day) {
+//     case 'Понеділок':
+//         alert(`Сьогодні ${day}`)
+//         break;
+
+//     case 'Вівторок':
+//         alert(`Сьогодні ${day}`)
+//         break;
+        
+//     case 'Середа':
+//         alert(`Сьогодні ${day}`)
+//         break;
+
+//     case 'Четвер':
+//         alert(`Сьогодні ${day}`)
+//         break;
+
+//     case 'П`ятниця':
+//         alert(`Сьогодні ${day}`)
+//         break;
+        
+//     case 'Субота':
+//         alert(`Сьогодні ${day}`)
+//         break;
+
+//     case 'Неділя':
+//         alert(`Сьогодні ${day}`)
+//         break;  
+
+//     default:
+//         break;
+// }
+
+// const month = prompt('Введіть номер місяця. Наприклад: 1');
+// const monthNum = Number(month); 
+
+// switch (monthNum) {
+//     case 12:
+//     case 1:
+//     case 2: 
+//         alert('Зараз зима')
+//         break;
+//     case 3:
+//     case 4:
+//     case 5: 
+//         alert('Зараз весна')
+//         break;
+
+//     case 6:
+//     case 7:
+//     case 8:    
+//         alert('Зараз літо')
+//         break;
+
+//     case 9:
+//     case 10:
+//     case 11: 
+//         alert('Зараз осінь')
+//         break;    
+
+//     default:
+//         break;
+// }
+
