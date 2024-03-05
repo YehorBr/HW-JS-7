d9d4ef11c88e87fa1454bf0b7374c97c4f1a9ad2
Yehor <yehor12344321@gmail.com>
2024-03-05 17:52:26 +0200

    зберігає зміни

--- index.js
+++ index.js
@@ -83,3 +83,90 @@
 //         break;
 // }
 
+// const month = prompt('Введіть номер місяця. Наприклад: 1');
+// const monthNum = Number(month); 
+
+// switch (monthNum) {
+//     case 1:
+//     case 3:
+//     case 5:
+//     case 7:
+//     case 8:
+//     case 10:
+//     case 12:            
+//         alert("У цьому місяці 31 день")
+//         break;
+
+//     case 4:
+//     case 6:
+//     case 9:
+//     case 11:            
+//         alert("У цьому місяці 30 днів")
+//         break;
+        
+//     case 2:
+//         alert("У цьому місяці 28 або 29 днів")
+//         break;
+        
+//     default:
+//         break;
+// }
+
+
+// const color = prompt('Введіть один з кольорів світлофору:');
+
+// switch(color){
+//     case 'Червоний':
+//     case 'червоний':
+//         alert('Стоп')
+//         break;
+
+//     case 'Жовтий':
+//     case 'жовтий':
+//         alert('Чекати')
+//         break;
+
+//     case 'Зелений':
+//     case 'зелений':
+//         alert('Йти')
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+// const firstNum = prompt('Введіть перше число:')
+// const firstNumToTypeNumber = Number(firstNum)
+// const secondNum = prompt('Введіть друге число:')
+// const secondNumToTypeNumber = Number(secondNum)
+
+
+// const mathSymbol = prompt('Введіть один з знаків: +, -, *, /. ')
+
+// switch(mathSymbol){
+//     case '+':
+//         const sumOfNums = firstNumToTypeNumber + secondNumToTypeNumber;
+//         alert(`Сума ваших чисел: ${sumOfNums}`);
+//         break;
+    
+//     case '-':
+//         const differenceOfNums = firstNumToTypeNumber - secondNumToTypeNumber;
+//         alert(`Різниця ваших чисел: ${differenceOfNums}`);
+//         break;
+        
+//     case '*':
+//         const multiplicationOfNums = firstNumToTypeNumber * secondNumToTypeNumber;
+//         alert(`Множення ваших чисел: ${multiplicationOfNums}`);
+//         break;
+
+//     case '/':
+//         const divisionOfNums = firstNumToTypeNumber / secondNumToTypeNumber;
+//         alert(`Ділення ваших чисел: ${divisionOfNums}`);
+//         break;    
+
+//         default:
+//             break;
+// }
+
+
